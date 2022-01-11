d18bfaf3dc1f0c3152d4b32df8a7dca6ecb0f59b
Elizar <elizar.aitbek@gmail.com>
2022-01-11 17:58:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("You are gay?", "Enter your answer...");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('How are you?');
+// answers[1] = +prompt('How old are you?');
+// answers[2] = prompt('How it going?');
+
+// console.log(answers);
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+
+// const name = "Ivan";
+// alert(`Good morning, ${name}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++; // постфиксная запись
+--decr; // префиксная запись
+
+console.log(incr);
+console.log(decr);
